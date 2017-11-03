597ad887121d20f33060ff1693e6589a2973bd5d
Jayderade <jcastleden@gmail.com>
2017-11-03 12:09:31 +1100

    CATCH up

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[AddComponentMenu("Character Set Up/Character Movement")]
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+
+    #region Variables
+    [Header("Characters MoveDirection")]
+    Vector3 moveDir = Vector3.zero;
+    private CharacterController charC;
+
+    [Header("Character Variables")]
+    public float jumpSpeed = 8f;
+    public float speed = 6f;
+    public float gravity = 20f;
+    #endregion
+    #region Start
+    void Start()
+    {
+        charC = this.GetComponent<CharacterController>();
+    }
+    //charc is on this game object we need to get the character controller that is attached to it
+    #endregion
+    #region Update
+    void Update()
+    {
+
+
+        //if our character is grounded
+        if (charC.isGrounded)
+        {
+            //we are able to move in game scene meaning
+            //Input Manager(https://docs.unity3d.com/Manual/class-InputManager.html)
+            //Input(https://docs.unity3d.com/ScriptReference/Input.html)
+            //moveDir is equal to a new vector3 that is affected by Input.Get Axis.. Horizontal, 0, Vertical
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            //moveDir is transformed in the direction of our moveDir
+            moveDir = transform.TransformDirection(moveDir);
+            //our moveDir is then multiplied by our speed
+            moveDir *= speed;
+            //we can also jump if we are grounded so
+            //in the input button for jump is pressed then
+            if (Input.GetButton("Jump"))
+            {
+                //our moveDir.y is equal to our jump speed
+                moveDir.y = jumpSpeed;
+            }
+        }
+        //regardless of if we are grounded or not the players moveDir.y is always affected by gravity timesed my time.deltaTime to normalize it
+        moveDir.y -= gravity * Time.deltaTime;
+        //we then tell the character Controller that it is moving in a direction timesed Time.deltaTime
+        charC.Move(moveDir * Time.deltaTime);
+    }
+}
+#endregion
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[AddComponentMenu("Character Set Up/Interact")]
+public class PickUp : MonoBehaviour {
+
+    #region Variables
+    //We are setting up these variable and the tags in update for week 3,4 and 5
+    [Header("Player and Camera connection")]
+    //create two gameobject variables one called player and the other mainCam
+    public GameObject player;
+    public GameObject mainCam;
+    #endregion
+    #region Start
+    private void Start()
+    {
+        //connect our player to the player variable via tag
+        player = GameObject.FindGameObjectWithTag("Player");
+        //connect our Camera to the mainCam variable via tag
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+    #endregion
+    #region Update
+    private void Update()
+    {
+        //if our interact key is pressed
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //create a ray
+            Ray interact;
+            //this ray is shooting out from the main cameras screen point center of screen
+            interact = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+            //create hit info
+            RaycastHit hitInfo;
+            //if this physics raycast hits something within 10 units
+            if (Physics.Raycast(interact, out hitInfo, 10))
+            {
+                #region NPC tag
+                //and that hits info is tagged NPC
+                if (hitInfo.collider.CompareTag("NPC"))
+                {
+                    //Debug that we hit a NPC
+                    Debug.Log("Hit the NPC");
+                }
+                #endregion
+                #region Item
+                //and that hits info is tagged Item
+                if (hitInfo.collider.CompareTag("Item"))
+                {
+                    //Debug that we hit an Item
+                    Debug.Log("Hit an Item");
+                }
+                #endregion
+            }
+        }
+    }
+    #endregion
+}
